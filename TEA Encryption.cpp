44ef4765366aa0e20a9eae4aea75443376a0acb3
Josiah Lansford <redrobbin99@gmail.com>
2020-09-22 15:47:33 -0400

    made changes

--- TEA Encryption.cpp
+++ TEA Encryption.cpp
@@ -35,6 +35,12 @@ int main()
 
     // read in ciphertext
     cipherFile.open(fileName);
+    if (!cipherFile.is_open()) {
+        std::cout << "Unable to open cipher file.";
+        return 1;
+    }
+
+
         
 }
 
@@ -42,6 +48,21 @@ std::string decrypt(int L, int R, unsigned int K[KEY_SIZE]) {
     return "";
 }
 
+
+/*
+Decryption:
+Assuming 32 rounds:
+(K[0], K[1], K[2], K[3]) = 128 bit key
+(L, R) = ciphertext(64 - bit block)
+delta = 0x9e3779b9
+sum = delta << 5
+for i = 1 to 32     
+  R -= ((L << 4) + K[2]) ^ (L + sum) ^ ((L >> 5) + K[3])
+  L -= ((R << 4) + K[0]) ^ (R + sum) ^ ((R >> 5) + K[1]) 
+  sum -= delta
+next i
+plaintext = (L, R)
+*/
 
 
 /*
@@ -57,20 +78,5 @@ for i = 1 to 32
     R += ((L<<4)+K[2])^(L+sum)^((L>>5)+K[3])
 next i
 ciphertext = (L,R)
-*/
-
-/*
-Decryption:
-Assuming 32 rounds:
-(K[0], K[1], K[2], K[3]) = 128 bit key
-(L, R) = ciphertext(64 - bit block)
-delta = 0x9e3779b9
-sum = delta << 5
-for i = 1 to 32     
-  R -= ((L << 4) + K[2]) ^ (L + sum) ^ ((L >> 5) + K[3])
-  L -= ((R << 4) + K[0]) ^ (R + sum) ^ ((R >> 5) + K[1]) 
-  sum -= delta
-next i
-plaintext = (L, R)
 */
 
