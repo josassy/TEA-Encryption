0c07b522078d2ee4e7946be9c1169ba31e7693fe
Josiah Lansford <redrobbin99@gmail.com>
2020-09-22 13:23:21 -0400

    write hex read-in function

--- TEA Encryption.cpp
+++ TEA Encryption.cpp
@@ -1,20 +1,76 @@
-// TEA Encryption.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <string>
 
+const int KEY_SIZE = 4;
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::ifstream cipherFile;
+    std::string fileName;
+    std::cout << "enter filename to decrypt: ";
+    std::cin >> fileName;
+
+    std::ifstream keyFile;
+    std::string line;
+    keyFile.open("teacher-H.key");
+    if (keyFile.is_open()) {
+        getline(keyFile, line);
+    }
+    else {
+        std::cout << "Unable to open key file.";
+        return 1;
+    }
+    keyFile.close();
+    std::cout << line;
+
+    // split 128-bit key into 4 integers
+    unsigned int K[KEY_SIZE];
+    for (int i = 0; i < KEY_SIZE; i++) {
+        std::string substring = line.substr(i * 8, 8);
+        K[i] = std::stoul(substring, nullptr, 16);
+     
+        // test to see if integer was read in correctly:
+        //printf("\n%X", K[i]);
+    }
+
+    // read in ciphertext
+    cipherFile.open(fileName);
+        
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+std::string decrypt(int L, int R, unsigned int K[KEY_SIZE]) {
+    return "";
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+/*
+Encryption:
+Assuming 32 rounds:
+(K[0],K[1],K[2],K[3]) = 128 bit key
+(L,R) = plaintext (64-bit block)
+delta = 0x9e3779b9
+sum = 0
+for i = 1 to 32     
+    sum += delta     
+    L += ((R<<4)+K[0])^(R+sum)^((R>>5)+K[1]) 
+    R += ((L<<4)+K[2])^(L+sum)^((L>>5)+K[3])
+next i
+ciphertext = (L,R)
+*/
+
+/*
+Decryption:
+Assuming 32 rounds:
+(K[0], K[1], K[2], K[3]) = 128 bit key
+(L, R) = ciphertext(64 - bit block)
+delta = 0x9e3779b9
+sum = delta << 5
+for i = 1 to 32     
+  R -= ((L << 4) + K[2]) ^ (L + sum) ^ ((L >> 5) + K[3])
+  L -= ((R << 4) + K[0]) ^ (R + sum) ^ ((R >> 5) + K[1]) 
+  sum -= delta
+next i
+plaintext = (L, R)
+*/
+
